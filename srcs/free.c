4d9f703ce27c48f803537cad0fe6c3a84b80da2d
Antonin <antonin@Lunas-MacBook-Air.local>
2018-11-23 10:53:38 -0800

    [ADD] Refactoring on free.c

--- srcs/free.c
+++ srcs/free.c
@@ -12,23 +12,9 @@
 
 #include "malloc.h"
 
-static void			free_unused_mem(const int malloc_size, t_page *mem)
+/*static void			free_unused_mem(const int malloc_size, t_page *mem)
 {
-	size_t const	zone_sizes[2] = {ZONE_TINY, ZONE_SMALL};
-
-	if (mem->prev)
-		mem->prev->next = mem->next;
-	else
-	{
-		if (malloc_size)
-			g_malloc_pages.tiny = mem->next;
-		else
-			g_malloc_pages.small = mem->next;
-	}
-	if (mem->next)
-		mem->next->prev = mem->prev;
-	munmap(mem, MALLOC_ZONE * zone_sizes[malloc_size]);
-}
+}*/
 
 static inline void	free_not_large(t_block *block, \
 						const int malloc_size, t_page *mem)
@@ -45,8 +31,23 @@ static inline void	free_not_large(t_block *block, \
 		mem->free->prev = block;
 	mem->free = block;
 	if (!mem->alloc)
-		free_unused_mem(malloc_size, mem);
+	{
+		if (mem->prev)
+			mem->prev->next = mem->next;
+		else if (malloc_size)
+				g_malloc_pages.tiny = mem->next;
+		else
+				g_malloc_pages.small = mem->next;
+		if (mem->next)
+			mem->next->prev = mem->prev;
+		munmap(mem, MALLOC_ZONE << (6 + (malloc_size << 2)));
+	}
 }
+
+/*
+** the large block is a big one
+** we free the area aligned on 4096 bytes
+*/
 
 static inline void	free_large(t_block *block)
 {
@@ -61,6 +62,11 @@ static inline void	free_large(t_block *block)
 	munmap(block, msize);
 }
 
+/*
+** We free block according to the types
+** g_malloc_pages.tiny + sizeof(t_page) * type' is used to access tiny or small
+*/
+
 static void			free_block(t_block *block)
 {
 	const int		type = page_size(block->size);
@@ -71,9 +77,9 @@ static void			free_block(t_block *block)
 		free_large(block);
 	else
 	{
-		mem = (!type) ? g_malloc_pages.tiny : g_malloc_pages.small;
-		while (!((void *)block < (void *)mem + MALLOC_ZONE * \
-			zone_sizes[type] && (void *)block > (void *)mem))
+		mem = g_malloc_pages.tiny + sizeof(t_page) * type;
+		while ((void *)mem + MALLOC_ZONE * zone_sizes[type] <= (void *)block \
+						&& (void *)block <= (void *)mem)
 			mem = mem->next;
 		free_not_large(block, type, mem);
 	}
